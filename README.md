732f976e4dd1af3aa3c1bb3bcbf993f47b14deb5
vicluq <victoria.tewari@gmail.com>
2023-09-12 09:43:41 -0300

    feat: UI header creation

---
+++ README.md
@@ -0,0 +1,10 @@
+# NLW IA
+
+## Front
+
+### Stack
+
+* React w/ Vite
+* Tailwind CSS
+* Radix UI (component library without UI styling, only functionalities)
+* shadcn/ui (component lib styled with tailwind)
--- nlw-ia-front/src/App.tsx
+++ nlw-ia-front/src/App.tsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+import { Button } from '@/components/ui/button';
+import { Separator } from './components/ui/separator';
+import { Github } from 'lucide-react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Hello Upload.AI</h1>
+    <div>
+      <div id='AppHeader' className='px-6 py-3 flex items-center justify-between'>
+        <h1 className='text-xl font-bold'>upload.ai</h1>
+
+        <div className='flex items-center gap-3'>
+          <span className='text-sm text-muted-foreground'>Desenvolvido no NLW da Rocketseat</span>
+          
+          <Separator orientation='vertical' className='h-6' />
+          
+          <Button variant="outline">
+            <Github className='w-4 h-4 mr-2' />
+            Github
+          </Button>
+        </div>
+      </div>
+
+      
+    </div>
   )
 }
 
